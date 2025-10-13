18792cdc17dc9ee70de817f4b14861ea79b6b9dc
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-10-13 17:27:44 +0200

    open gui

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
+use fltk::{app, prelude::*, window::Window};
 use wifiscanner::{self, scan};
 use chrono::{self, Utc};
 
 fn main() {
-    let time_format = "%Y-%m-%d %H:%M:%S";
-    let mut current_time;
-    let mut scanner_result;
-    loop {
-        scanner_result = scan();
-        current_time = Utc::now().format(time_format);
-        println!("Time: {}", current_time);
-        match scanner_result {
-            Ok(wifis) => {
-                if wifis.get(0) != None {
-                    println!("\nDetected {} Networks at {}: ", wifis.len(), current_time);
-                    for single_wifi in &wifis {
-                        println!("{}", single_wifi.ssid)
-                    }
-                } else {
-                    println!("No Networks detected.");
-                    println!("Please check your WiFi Adapter.");
-                    std::process::exit(1);
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 500, "Signal Locate");
+    wind.end();
+    wind.show();
+    app.run().unwrap();
+}
+
+fn get_networks() -> i32 {
+    let time_format: &'static str = "%Y-%m-%d %H:%M:%S";
+    let scanner_result = scan();
+    let current_time = Utc::now().format(time_format);
+    println!("Time: {}", current_time);
+    match scanner_result {
+        Ok(wifis) => {
+            if wifis.get(0) != None {
+                println!("\nDetected {} Networks at {}: ", wifis.len(), current_time);
+                for single_wifi in &wifis {
+                    println!("{}", single_wifi.ssid)
                 }
+                return 0;
+            } else {
+                println!("No Networks detected.");
+                println!("Please check your WiFi Adapter.");
+                return 1;
             }
-            Err(e) => {
-                println!("Scan failed: {:?}", e)
-            }
+        }
+        Err(e) => {
+            println!("Scan failed: {:?}", e);
+            return 1;
         }
     }
 }
