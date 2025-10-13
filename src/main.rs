69082feaea70d143313b5dbd8d6fa8ba68f6e107
simon0302010 <simon0302010@gmail.com>
2025-10-13 15:44:38 +0200

    loop in main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
-use wifiscanner;
+use wifiscanner::{self, scan};
+use chrono::{self, Utc};
 
 fn main() {
-    let scanner_result = wifiscanner::scan();
-    match scanner_result {
-        Ok(wifis) => {
-            if wifis.get(0) != None {
-                println!("Detected Networks: ");
-                for single_wifi in wifis {
-                    println!("{}", single_wifi.ssid)
+    let time_format = "%Y-%m-%d %H:%M:%S";
+    let mut current_time;
+    let mut scanner_result;
+    loop {
+        scanner_result = scan();
+        current_time = Utc::now().format(time_format);
+        println!("Time: {}", current_time);
+        match scanner_result {
+            Ok(wifis) => {
+                if wifis.get(0) != None {
+                    println!("\nDetected {} Networks at {}: ", wifis.len(), current_time);
+                    for single_wifi in &wifis {
+                        println!("{}", single_wifi.ssid)
+                    }
                 }
             }
-        }
-        Err(e) => {
-            println!("Scan failed: {:?}", e)
+            Err(e) => {
+                println!("Scan failed: {:?}", e)
+            }
         }
     }
 }
