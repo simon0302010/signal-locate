6baa02126b31a0fa4824aaaa068dbf578928dd26
simon0302010 <simon0302010@gmail.com>
2025-10-14 13:34:16 +0200

    more image formats

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use fltk::{app, button::Button, prelude::*, window::Window, dialog, image::{PngImage, JpegImage}, frame};
+use fltk::{app, button::Button, dialog, frame::{self, Frame}, image::{JpegImage, PngImage, SvgImage}, prelude::*, window::Window};
 use wifiscanner::{self, scan, Wifi};
 use chrono::{self, Utc};
+use std::{rc::Rc, cell::RefCell};
 
 fn main() {
     let app = app::App::default()
@@ -10,16 +11,23 @@ fn main() {
         .with_size(80, 30)
         .with_pos(210, 20)
         .with_label("Open File");
-    let mut image_frame = frame::Frame::new(0, 60, 500, 440, "");
+    let image_frame = Rc::new(RefCell::new(Frame::new(0, 60, 500, 440, "")));
     wind.make_resizable(true);
     wind.size_range(450, 350, 0, 0);
     wind.end();
     wind.show();
+
+    let image_frame_resize = Rc::clone(&image_frame);
+    wind.resize_callback(move |_, _, _, w, h| {
+        image_frame_resize.borrow_mut().set_size(w, h - 60);
+    });
+
+    let image_frame_button = Rc::clone(&image_frame);
     button.set_callback(move |_| {
         let file_path = choose_file();
         println!("{:?}", file_path);
         if let Some(path) = file_path {
-            set_image(&path, &mut image_frame);
+            set_image(&path, &mut image_frame_button.borrow_mut());
         }
     });
     app.run().unwrap();
@@ -28,7 +36,7 @@ fn main() {
 fn choose_file() -> Option<String> {
     let mut chooser = dialog::FileChooser::new(
         ".",
-        "*.{png,jpg}",
+        "*.{png,jpg,svg}",
         dialog::FileChooserType::Multi,
         "Select Room plan",
     );
@@ -44,7 +52,7 @@ fn set_image(image_path: &str, image_frame: &mut frame::Frame) {
     if image_path.ends_with(".png") {
         match PngImage::load(image_path) {
             Ok(img) => {
-                image_frame.set_image_scaled(Some(img));
+                image_frame.set_image(Some(img));
             }
             Err(e) => {
                 println!("An error occurred: {:?}", e)
@@ -53,7 +61,16 @@ fn set_image(image_path: &str, image_frame: &mut frame::Frame) {
     } else if image_path.ends_with(".jpg") || image_path.ends_with(".jpeg") {
         match JpegImage::load(image_path) {
             Ok(img) => {
-                image_frame.set_image_scaled(Some(img));
+                image_frame.set_image(Some(img));
+            }
+            Err(e) => {
+                println!("An error occurred: {:?}", e)
+            }
+        }
+    } else if image_path.ends_with(".svg") {
+        match SvgImage::load(image_path) {
+            Ok(img) => {
+                image_frame.set_image(Some(img));
             }
             Err(e) => {
                 println!("An error occurred: {:?}", e)
