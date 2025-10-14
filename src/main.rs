54e7bd854d5cf987f6ac26c0480b08640b530c3d
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-10-14 17:05:55 +0200

    scaling support for images

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use fltk::{app, button::Button, dialog, frame::{self, Frame}, image::{JpegImage, PngImage, SvgImage}, prelude::*, window::Window};
+use fltk::{app, button::Button, dialog, frame::{self, Frame}, image::{JpegImage, PngImage, SvgImage, SharedImage}, prelude::*, window::Window};
 use wifiscanner::{self, scan, Wifi};
 use chrono::{self, Utc};
 use std::{rc::Rc, cell::RefCell};
 
 fn main() {
+    let file_path: Rc<RefCell<Option<String>>> = Rc::new(RefCell::new(None));
+    let cached_image: Rc<RefCell<Option<SharedImage>>> = Rc::new(RefCell::new(None));
+
     let app = app::App::default()
         .with_scheme(app::Scheme::Gtk);
     let mut wind = Window::new(100, 100, 500, 400, "Signal Locate");
@@ -18,22 +21,44 @@ fn main() {
     wind.show();
 
     let image_frame_resize = Rc::clone(&image_frame);
+    let cached_img_resize = Rc::clone(&cached_image);
     wind.resize_callback(move |_, _, _, w, h| {
         image_frame_resize.borrow_mut().set_size(w, h - 60);
+        if let Some(ref img) = *cached_img_resize.borrow() {
+            let mut img = img.clone();
+            img.scale(w, h -60, true, true);
+            image_frame_resize.borrow_mut().set_image(Some(img));
+        }
     });
 
     let image_frame_button = Rc::clone(&image_frame);
+    let file_path_button = Rc::clone(&file_path);
+    let cached_image_button = Rc::clone(&cached_image);
     button.set_callback(move |_| {
-        let file_path = choose_file();
-        println!("{:?}", file_path);
-        if let Some(path) = file_path {
-            set_image(&path, &mut image_frame_button.borrow_mut());
+        let new_path = choose_file();
+        println!("{:?}", new_path);
+        *file_path_button.borrow_mut() = new_path;
+        if let Some(path) = file_path_button.borrow().as_ref() {
+            // load and cache
+            if let Ok(img) = fltk::image::SharedImage::load(&path) {
+                *cached_image_button.borrow_mut() = Some(img);
+            } else {
+                *cached_image_button.borrow_mut() = None;
+            }
+            // draw
+            if let Some(ref img) = *cached_image_button.borrow() {
+                let mut img = img.clone();
+                let frame_w = image_frame_button.borrow().width();
+                let frame_h = image_frame_button.borrow().height();
+                img.scale(frame_w, frame_h, true, true);
+                image_frame_button.borrow_mut().set_image(Some(img));
+            }
         }
     });
     app.run().unwrap();
 }
 
-fn choose_file() -> Option<String> {
+fn choose_file() -> Option<String>{
     let mut chooser = dialog::FileChooser::new(
         ".",
         "*.{png,jpg,svg}",
@@ -49,9 +74,13 @@ fn choose_file() -> Option<String> {
 }
 
 fn set_image(image_path: &str, image_frame: &mut frame::Frame) {
+    let frame_w = image_frame.width();
+    let frame_h = image_frame.height();
+
     if image_path.ends_with(".png") {
         match PngImage::load(image_path) {
-            Ok(img) => {
+            Ok(mut img) => {
+                img.scale(frame_w, frame_h, true, true);
                 image_frame.set_image(Some(img));
             }
             Err(e) => {
@@ -60,7 +89,8 @@ fn set_image(image_path: &str, image_frame: &mut frame::Frame) {
         }
     } else if image_path.ends_with(".jpg") || image_path.ends_with(".jpeg") {
         match JpegImage::load(image_path) {
-            Ok(img) => {
+            Ok(mut img) => {
+                img.scale(frame_w, frame_h, true, true);
                 image_frame.set_image(Some(img));
             }
             Err(e) => {
@@ -69,7 +99,8 @@ fn set_image(image_path: &str, image_frame: &mut frame::Frame) {
         }
     } else if image_path.ends_with(".svg") {
         match SvgImage::load(image_path) {
-            Ok(img) => {
+            Ok(mut img) => {
+                img.scale(frame_w, frame_h, true, true);
                 image_frame.set_image(Some(img));
             }
             Err(e) => {
